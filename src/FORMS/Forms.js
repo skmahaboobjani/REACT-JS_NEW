cc0cb73e5664a096794947764bbf28a633148ce1
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2025-11-13 14:23:48 +0530

    Update Forms.js

--- src/FORMS/Forms.js
+++ src/FORMS/Forms.js
@@ -1,53 +1,117 @@
-import Reat, { useState } from "react"
+// import Reat, { useState } from "react"
 
-function Forms()
-{
-    const [data,setdata]=useState({names:[],fullname:[],age:[]})
-    const [list,setlist]=useState([])
+// function Forms()
+// {
+//     const [data,setdata]=useState({names:[],fullname:[],age:[]})
+//     const [list,setlist]=useState([])
 
-    let handlechange=(e)=>
-    {
+//     let handlechange=(e)=>
+//     {
 
-        let value=e.target.value
-        let name=e.target.name
-        setdata((pv)=>({
+//         let value=e.target.value
+//         let name=e.target.name
+//         setdata((pv)=>({
 
-            ...pv,
-            [name]:value
+//             ...pv,
+//             [name]:value
 
 
 
-        }))
+//         }))
 
-    }
+//     }
 
-    let handel=()=>
-    {
-       setlist([...list,data])
-    }
-    return(
-        <div>
-   <label>USERNAME</label><br/>
-            <input type={"text"} name={"names"} onChange={handlechange}/><br/>
-            <label>Full Name</label><br/>
-            <input type={"text"} name={"fullname"} onChange={handlechange}/><br/>
-            <label>age</label><br/>
-            <input type={"number"} name={"age"} onChange={handlechange}/><br/>
-            <button onClick={handel}>submit</button>
+//     let handel=()=>
+//     {
+//        setlist([...list,data])
+//     }
+//     return(
+//         <div>
+//    <label>USERNAME</label><br/>
+//             <input type={"text"} name={"names"} onChange={handlechange}/><br/>
+//             <label>Full Name</label><br/>
+//             <input type={"text"} name={"fullname"} onChange={handlechange}/><br/>
+//             <label>age</label><br/>
+//             <input type={"number"} name={"age"} onChange={handlechange}/><br/>
+//             <button onClick={handel}>submit</button>
 
-         {list.map((x)=>
-        (
-            <>
-            {x.names}<br/>
-            {x.fullname}<br/>
-            {x.age}
-            </>
+//          {list.map((x)=>
+//         (
+//             <>
+//             {x.names}<br/>
+//             {x.fullname}<br/>
+//             {x.age}
+//             </>
             
-         ))}
+//          ))}
 
 
-        </div>
-    )
 
+
+
+
+
+
+
+
+import React, { useState } from "react";
+function App() {
+  const [data, setdata] = useState({ name: [], email: [], number: [] });
+  const [err, seterr] = useState("");
+
+  let handlechange = (e) => {
+    const { name, value } = e.target;
+    setdata((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const submit = () => {
+    if (!data.name || !data.email || !data.number) {
+      seterr("please enter all details before submitting");
+    } else {
+      seterr("");
+      console.log(data);
+
+      alert("form submitted succesfully");
+    }
+  };
+  return (
+    <div>
+      <h1>forms</h1>
+      <input
+        type={"text"}
+        value={data.name}
+        name={"name"}
+        onChange={handlechange}
+      />
+      <br />
+      <input
+        type={"email"}
+        value={data.email}
+        name={"email"}
+        onChange={handlechange}
+      />
+      <br />
+      <input
+        type={"number"}
+        value={data.number}
+        name={"number"}
+        onChange={handlechange}
+      />
+      <br />
+      <button onClick={submit}>submit</button>
+      <br />
+      {err}
+    </div>
+  );
 }
-export default Forms
+export default App;
+
+
+//         </div>
+//     )
+
+// }
+// export default Forms
