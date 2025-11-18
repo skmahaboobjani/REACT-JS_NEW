a6b18de5d050344cb087cab1a657881d9a77654c
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2025-11-18 10:45:04 +0530

    Update Tab.js

--- src/TAB_ADVANCES/Tab.js
+++ src/TAB_ADVANCES/Tab.js
@@ -1,7 +1,77 @@
+// import React, { useState } from "react";
+
+// function App() 
+// {
+//   const obj = [
+//     {
+//       title: "TAB1",
+//       content: "frontend",
+//     },
+//     {
+//       title: "TAB2",
+//       content: "backend",
+//     },
+//     {
+//       title: "TAB3",
+//       content: "full stack",
+//     },
+//   ];
+
+//   const [selectedTab, setSelectedTab] = useState([]);
+
+//   const handleSelect = (title) =>
+//    {
+  
+//     if (selectedTab.includes(title))
+//      {
+        
+//       setSelectedTab(selectedTab.filter((t) => t !== title));
+//     } else {
+        
+//     //   setSelectedTab([...selectedTab, title]);
+//     setSelectedTab([title])
+     
+//     }
+//     console.log(title) 
+   
+//   };
+
+
+
+//   return (
+//     <div>
+//       {obj.map((x) => (
+//         <div
+//           key={x.title}
+//           style={{ backgroundColor: selectedTab.includes(x.title) ? "red" : "yellow" }}
+//           onClick={() => handleSelect(x.title)}
+//         >
+//           {x.title}
+//         </div>
+//       ))}
+
+//       <div>
+//      {selectedTab.map((x)=>
+//      {
+//         let contents=obj.find((m,n)=>m.title===x)
+
+//         return(
+//              <div>
+//                 {contents.content}
+//                 </div>
+//         )
+//      })}
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 
-function App() 
-{
+function App() {
   const obj = [
     {
       title: "TAB1",
@@ -17,56 +87,58 @@ function App()
     },
   ];
 
-  const [selectedTab, setSelectedTab] = useState([]);
+  const [select, setselect] = useState([]);
 
-  const handleSelect = (title) =>
-   {
-  
-    if (selectedTab.includes(title))
-     {
-        
-      setSelectedTab(selectedTab.filter((t) => t !== title));
+  let handle = (title) => {
+    if (select.includes(title)) {
+      setselect(select.filter((x) => x != title));
     } else {
-        
-    //   setSelectedTab([...selectedTab, title]);
-    setSelectedTab([title])
-     
+      setselect([title]);
     }
-    console.log(title) 
-   
   };
 
-
-
+  let filters = select.map((x) => obj.find((m) => m.title === x));
   return (
     <div>
       {obj.map((x) => (
-        <div
-          key={x.title}
-          style={{ backgroundColor: selectedTab.includes(x.title) ? "red" : "yellow" }}
-          onClick={() => handleSelect(x.title)}
+        <li
+          style={{
+            backgroundColor: select.includes(x.title) ? "red" : "green",
+          }}
+          onClick={() => handle(x.title)}
         >
           {x.title}
-        </div>
+        </li>
       ))}
-
-      <div>
-     {selectedTab.map((x)=>
-     {
-        let contents=obj.find((m,n)=>m.title===x)
-
-        return(
-             <div>
-                {contents.content}
-                </div>
-        )
-     })}
-
-      </div>
+      <div>{filters[0].content}</div>
     </div>
   );
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
